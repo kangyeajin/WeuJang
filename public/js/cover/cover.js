3bfb5e1fbe5bc65e6b5bdf448efbb6aa3c4c1dac
kkbg52 <kkbg52@gamil.com>
2025-05-08 00:22:00 +0900

    [25.05.08] 가림판 표출위치 변경

--- public/js/cover/cover.js
+++ public/js/cover/cover.js
@@ -51,5 +51,18 @@ document.addEventListener("touchend", () => {
 
 // 토글
 document.getElementById('coverfg').addEventListener('click', function () {
-  cover.classList.toggle('show');
+  if (!cover.classList.contains('show')) {
+    cover.classList.add('show'); // 일단 보여야 크기 측정 가능
+
+    // 한 프레임 기다렸다가 위치 설정
+    requestAnimationFrame(() => {
+      const x = window.scrollX + window.innerWidth / 2 - cover.offsetWidth / 2;
+      const y = window.scrollY + window.innerHeight / 2 - cover.offsetHeight / 2;
+
+      cover.style.left = `${x}px`;
+      cover.style.top = `${y+50}px`;
+    });
+  } else {
+    cover.classList.remove('show');
+  }
 });
